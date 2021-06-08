ebc424ee61fdb63ef16be7af33cb295bdc207c97
Kate Bordasheva <artemissa.ks@gmail.com>
2021-06-08 21:43:17 +0300

    Task 4.5.2

---
+++ WebApplication_Bordasheva.Blazor/Data/ApiViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace WebApplication_Bordasheva.Blazor.Data
+{
+    public class ListViewModel
+    {
+        [JsonPropertyName("phoneId")]
+        public int PhoneId { get; set; } // id телефона 
+        [JsonPropertyName("phoneName")]
+        public string PhoneName { get; set; } // название телефона 
+
+    }
+
+    public class DetailsViewModel
+    {
+        [JsonPropertyName("phoneName")]
+        public string PhoneName { get; set; } // название телефона
+        [JsonPropertyName("description")]
+        public string Description { get; set; } // описание телефона 
+        [JsonPropertyName("price")]
+        public int Calories { get; set; } // цена
+        [JsonPropertyName("image")]
+        public string Image { get; set; } // имя файла изображения    
+    }
+
+}
